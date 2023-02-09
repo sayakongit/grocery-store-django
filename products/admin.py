76a49f3a7d778805be3700f8c769f89f2dc1aa34
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-02-09 20:52:52 +0530

    added variant models

--- products/admin.py
+++ products/admin.py
@@ -3,3 +3,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(ColorVariant)
+admin.site.register(SizeVariant)
+admin.site.register(QuantityVariant)
--- products/models.py
+++ products/models.py
@@ -17,6 +17,25 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
     
+class SizeVariant(models.Model):
+    size_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.size_name
+
+class ColorVariant(models.Model):
+    color_name = models.CharField(max_length=100)
+    color_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.color_name
+
+class QuantityVariant(models.Model):
+    variant_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.variant_name
+    
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=100)
@@ -24,10 +43,11 @@ class Product(models.Model):
     stock = models.IntegerField(default=100)
     price = models.CharField(max_length=50)
     description = models.TextField()
+    
+    quantity_type = models.ForeignKey(QuantityVariant, blank=True, null=True, on_delete=models.PROTECT)
+    size_type = models.ForeignKey(SizeVariant, blank=True, null=True, on_delete=models.PROTECT)
+    color_type = models.ForeignKey(ColorVariant, blank=True, null=True, on_delete=models.PROTECT)
 
     
     def __str__(self):
         return self.product_name
-
-
-
