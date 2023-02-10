e4cf6671519acf337bae0bc9f541e85c5428f35c
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-02-10 11:46:44 +0530

    created product list api

--- products/models.py
+++ products/models.py
@@ -51,3 +51,10 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product ,on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='static/prouducts')
+
+    def __str__(self):
+        return self.product.product_name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .serializers import *
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class ProductView(APIView):
+    def get(self, request):
+        query_set = Product.objects.all()
+        serializer = ProductSerializer(query_set, many = True)
+        return Response(serializer.data)
